ebdaeb10acb303893cc6041d4ed6349463191e4b
vpulkit007 <pulkit.0092013@gmail.com>
2023-12-01 13:31:04 +0530

    commit 4

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
 
 
-/*const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const { exec } = require('child_process');
+const { type } = require('os');
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+let respon;
+
+shellCommand()
+
+app.get('', (req, res) => {
+
+    console.log(typeof respon);
+    res.send(respon);
 })
 
-app.listen(port, () => {
+/*app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })*/
 
 
 
 
-const { exec } = require('child_process');
 
-const commandToExecute = 'npx eslint lwc';
-exec(commandToExecute, (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Error: ${error.code}`);
-        console.error(`Error: ${error.signal}`);
-        //return;
-    }
+function shellCommand() {
 
-    if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        //return;
-    }
+    const commandToExecute = 'npx eslint lwc';
+    exec(commandToExecute, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.code}`);
+            console.error(`Error: ${error.signal}`);
+            //return;
+        }
 
-    console.log(`Output: ${stdout}`);
-});
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            //return;
+        }
+
+        console.log(`Output: ${stdout}`);
+        respon = stdout;
+
+    });
+
+}
+
