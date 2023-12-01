ec89fea0a6b04441fd21226a62e0c59eae271ec7
vpulkit007 <pulkit.0092013@gmail.com>
2023-12-01 13:57:24 +0530

    commit 5

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 
-
+const path = require('path')
 const express = require('express')
-
 const { exec } = require('child_process');
-const { type } = require('os');
+//const { type } = require('os');
 
 const app = express()
 const port = process.env.PORT || 5000;
+const publicPath = path.join(__dirname, 'public');
 
 let respon;
 
 shellCommand()
 
-app.get('', (req, res) => {
+/*app.get('', (req, res) => {
 
     console.log(typeof respon);
     res.send(respon);
-})
+})*/
+
+app.use(express.static(publicPath));
 
 /*app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
